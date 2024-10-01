6993a78c55b0f659afefa798347892afe8e3989a
Choi Do Yeun <87837828+ChoiDoYeun@users.noreply.github.com>
2024-10-02 08:53:45 +0900

    Update v2_pid.py

--- v2_pid.py
+++ v2_pid.py
@@ -172,6 +172,8 @@ def main():
             left_motor_speed = base_speed - pid_value  # 왼쪽 속도 제어
             right_motor_speed = base_speed + pid_value  # 오른쪽 속도 제어
 
+            print(f"left : {left_motor_speed} , right : {right_motor_speed}")
+
             # 모터 제어 함수 호출
             control_motors(left_motor_speed, right_motor_speed)
 
