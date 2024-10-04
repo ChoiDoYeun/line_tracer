0c9ea051b70cc761b01a2442ca154df8961644ab
Choi Do Yeun <87837828+ChoiDoYeun@users.noreply.github.com>
2024-10-04 21:22:12 +0900

    Update servo_test.py

--- servo_test.py
+++ servo_test.py
@@ -25,10 +25,13 @@ try:
     while True:
         # 0도, 90도, 180도로 이동
         set_angle(0)
+        print("0도")
         time.sleep(2)
         set_angle(90)
+        print("90도")
         time.sleep(2)
         set_angle(180)
+        print("180도")
         time.sleep(2)
 
 except KeyboardInterrupt:
