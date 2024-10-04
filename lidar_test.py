f42bfbed43a1bfd6015a8d9dda96200b2913b2e1
Choi Do Yeun <87837828+ChoiDoYeun@users.noreply.github.com>
2024-10-04 21:56:20 +0900

    Update lidar_test.py

--- lidar_test.py
+++ lidar_test.py
@@ -6,8 +6,8 @@ ser = serial.Serial('/dev/serial0', baudrate=115200, timeout=1)
 
 def read_distance():
     # 센서에서 데이터를 읽어들임
+    time.sleep(0.01)
     ser.write(b'\x42\x57\x02\x00\x00\x00\x01\x06')  # 데이터 요청 명령
-    time.sleep(0.1)  # 잠시 대기
     response = ser.read(9)  # 응답 데이터 읽기 (9바이트)
 
     if len(response) == 9:
