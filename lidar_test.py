018c46348ba6cf7baef6dcad630ec45c05e6136b
Choi Do Yeun <87837828+ChoiDoYeun@users.noreply.github.com>
2024-10-05 20:37:32 +0900

    Update lidar_test.py

--- lidar_test.py
+++ lidar_test.py
@@ -15,14 +15,13 @@ def update_scan(scan_data, laser):
         for point in scan_data.points:
             if -0.5236 <= point.angle <= 0.5236:  # -30도에서 +30도
                 # 거리 값이 0.1m ~ 8m 사이일 때만 유효한 데이터로 간주
-                if 0.1 <= point.range <= 8.0:
+                if 0.01 <= point.range <= 8.0:
                     angles.append(point.angle)
                     distances.append(point.range)
                     # 0도 근처 확인
                     if -0.01 <= point.angle <= 0.01:  # 0도 주변 데이터 출력
                         print(f"0도 근처 데이터 -> 각도: {point.angle:.2f} rad, 거리: {point.range:.2f} meters")
-                else:
-                    print(f"Out of range data -> 각도: {point.angle:.2f} rad, 거리: {point.range:.2f} meters (Filtered)")
+                        
         return angles, distances
     else:
         print("Failed to get Lidar Data.")
@@ -49,7 +48,7 @@ if __name__ == "__main__":
     laser.setlidaropt(ydlidar.LidarPropLidarType, ydlidar.TYPE_TOF)
     laser.setlidaropt(ydlidar.LidarPropDeviceType, ydlidar.YDLIDAR_TYPE_SERIAL)
     laser.setlidaropt(ydlidar.LidarPropScanFrequency, 5.0)
-    laser.setlidaropt(ydlidar.LidarPropSampleRate, 5000)
+    laser.setlidaropt(ydlidar.LidarPropSampleRate, 3000)
 
     # 단방향 통신 설정
     laser.setlidaropt(ydlidar.LidarPropSingleChannel, True)
@@ -63,7 +62,7 @@ if __name__ == "__main__":
         fig, ax = plt.subplots(subplot_kw={'projection': 'polar'})
         plot = ax.scatter([], [], s=5)
 
-        ax.set_ylim(0, 10)  # 거리의 범위를 미터 단위로 설정 (0~10m)
+        ax.set_ylim(0, 8)  # 거리의 범위를 미터 단위로 설정 (0~10m)
         ani = FuncAnimation(fig, animate, fargs=(laser, scan_data, plot), interval=100)
 
         plt.show()
