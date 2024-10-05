23a7c423e7c93cd8fc2d40d678bea8820ae386a0
Choi Do Yeun <87837828+ChoiDoYeun@users.noreply.github.com>
2024-10-06 08:21:46 +0900

    Update combine_v1.py

--- combine_v1.py
+++ combine_v1.py
@@ -175,7 +175,7 @@ def avoid_obstacle():
         print("우측으로 회피")
         control_motors(50, -50)  # 우측으로 회피
         time.sleep(0.001)  # 일정 시간 이동
-        print("좌측 최초 회피 끝")
+        print("우측 최초 회피 끝")
 
     # 이동 후 다시 좌우측 거리 재확인
     with obstacle_lock:
@@ -183,10 +183,13 @@ def avoid_obstacle():
         if left_distance > right_distance:
             print("계속 좌측으로 회피")
             control_motors(-50, 50)  # 좌측 회피 계속
-            
+            time.sleep(0.001)  # 일정 시간 이동
+            print("좌측 재 회피 꿑")
         else:
             print("우측으로 전환")
             control_motors(50, -50)  # 우측 회피로 전환
+            time.sleep(0.001)  # 일정 시간 이동
+            rint("우측 재 회피 꿑")
 
     time.sleep(1)  # 회피 완료 후 다시 이동
 
