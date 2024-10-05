9d59f7c7e6060d9b9ccaef7b560a913dd528ed27
Choi Do Yeun <87837828+ChoiDoYeun@users.noreply.github.com>
2024-10-05 18:20:45 +0900

    Update lidar_test.py

--- lidar_test.py
+++ lidar_test.py
@@ -1,25 +1,30 @@
-import sys
-from ydlidar import *
-
-def main():
-    laser = CYdLidar()
-    laser.setlidaropt(LidarPropSerialPort, "/dev/ttyUSB0")
-    laser.setlidaropt(LidarPropSerialBaudrate, 115200)
-    laser.setlidaropt(LidarPropLidarType, TYPE_TRIANGLE)
-    laser.setlidaropt(LidarPropDeviceType, YDLIDAR_TYPE_SERIAL)
-    laser.setlidaropt(LidarPropScanFrequency, 7.0)
-
-    if laser.initialize():
-        print("YDLidar initialized successfully!")
-        while True:
-            scan = laser.doProcessSimple(scan=None)
-            if scan:
-                print("Lidar scan received:", len(scan))
-    else:
-        print("Failed to initialize YDLidar.")
-    
-    laser.turnOff()
-    laser.disconnecting()
+import os
+import ydlidar
 
 if __name__ == "__main__":
-    main()
+    ydlidar.os_init();
+    laser = ydlidar.CYdLidar();
+    ports = ydlidar.lidarPortList();
+    port = "/dev/ydlidar";
+    for key, value in ports.items():
+        port = value;
+    laser.setlidaropt(ydlidar.LidarPropSerialPort, port);
+    laser.setlidaropt(ydlidar.LidarPropSerialBaudrate, 512000);
+    laser.setlidaropt(ydlidar.LidarPropLidarType, ydlidar.TYPE_TOF);
+    laser.setlidaropt(ydlidar.LidarPropDeviceType, ydlidar.YDLIDAR_TYPE_SERIAL);
+    laser.setlidaropt(ydlidar.LidarPropScanFrequency, 10.0);
+    laser.setlidaropt(ydlidar.LidarPropSampleRate, 20);
+    laser.setlidaropt(ydlidar.LidarPropSingleChannel, False);
+
+    ret = laser.initialize();
+    if ret:
+        ret = laser.turnOn();
+        scan = ydlidar.LaserScan()
+        while ret and ydlidar.os_isOk() :
+            r = laser.doProcessSimple(scan);
+            if r:
+                print("Scan received[",scan.stamp,"]:",scan.points.size(),"ranges is [",1.0/scan.config.scan_time,"]Hz");
+            else :
+                print("Failed to get Lidar Data.")
+        laser.turnOff();
+    laser.disconnecting();
