a6749a8122db6edf7dc50f512708631d2ac4221d
Choi Do Yeun <87837828+ChoiDoYeun@users.noreply.github.com>
2024-10-06 15:33:33 +0900

    Update v2_combine.py

--- v2_combine.py
+++ v2_combine.py
@@ -225,7 +225,7 @@ def avoid_obstacle_and_return():
         time.sleep(0.1)
         control_motors(50,-50) # 우회전
         time.sleep(0.90)
-        control_motors(40, 40) # 직진
+        control_motors(40, 40) # 직진 # 라인찾을때까지로 수정해야함 이후 라인을 찾으면 좌회전한후에 주행모드로 다시 돌아가야함
         time.sleep(1.29)
         
         motor1.stop()
@@ -244,32 +244,20 @@ def avoid_obstacle_and_return():
         time.sleep(0.1)        
         
     else:
-        print("우측 회피")
-        # 우측으로 회피하며 대각선 이동
-        control_motors(50, -50) # 우회전
-        time.sleep(0.65)  # 회피 시간 설정
-        control_motors(40, 40) # 직진
-        time.sleep(0.75)
+        control_motors(50, -50)
+        time.sleep(0.60)  # 회피 시간 설정
+        control_motors(40, 40)
+        time.sleep(1.25)
         motor1.stop()
         motor2.stop()
         motor3.stop()
         motor4.stop()
         time.sleep(0.1)
-        control_motors(-50,50) # 좌회전
-        time.sleep(0.65)
-        control_motors(40, 40) # 직진
-        time.sleep(1)
+        control_motors(-50,50) 
+        time.sleep(0.90)
         
-        motor1.stop()
-        motor2.stop()
-        motor3.stop()
-        motor4.stop()
-        time.sleep(0.1)
-        
-        control_motors(-50,+50) # 좌회전
-        time.sleep(0.65)
-        control_motors(40, 40) # 직진
-        time.sleep(1)
+        control_motors(40, 40) # 라인찾을때까지로 수정해야함 이후 라인을 찾으면 좌회전한후에 주행모드로 다시 돌아가야함
+        time.sleep(1.29)
         
         motor1.stop()
         motor2.stop()
@@ -277,16 +265,8 @@ def avoid_obstacle_and_return():
         motor4.stop()
         time.sleep(0.1)
         
-        control_motors(50, -50) # 우회전
-        time.sleep(0.55)
-        
-        motor1.stop()
-        motor2.stop()
-        motor3.stop()
-        motor4.stop()
-        time.sleep(0.1)     
-    
-    time.sleep(1)  # 라인 복귀 전 직진 시간 설정
+        control_motors(50, -50) # 좌회전
+        time.sleep(0.3)
 
     return
     
