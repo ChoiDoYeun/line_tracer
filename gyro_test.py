3b29326d5c094350b635be7abfe2928d6c286918
Choi Do Yeun <87837828+ChoiDoYeun@users.noreply.github.com>
2024-10-06 17:37:39 +0900

    Update gyro_test.py

--- gyro_test.py
+++ gyro_test.py
@@ -4,7 +4,7 @@ import adafruit_adxl34x
 
 # I2C 초기화
 i2c = busio.I2C(board.SCL, board.SDA)
-accelerometer = adafruit_adxl34x.ADXL345(i2c, address=0x1d)
+accelerometer = adafruit_adxl34x.ADXL345(i2c, address=0x1D)
 
 # 가속도 데이터 읽기
 while True:
