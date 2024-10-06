3fe13e4daf0ffedf3f9d65623bc95eacad47cc12
Choi Do Yeun <87837828+ChoiDoYeun@users.noreply.github.com>
2024-10-06 16:51:18 +0900

    Update motor_test.py

--- motor_test.py
+++ motor_test.py
@@ -74,14 +74,14 @@ try:
     motor4.stop()
     time.sleep(0.5)
     control_motors(100, -100)
-    time.sleep(0.8)
+    time.sleep(1)
     motor1.stop()
     motor2.stop()
     motor3.stop()
     motor4.stop()
     time.sleep(0.5)
     control_motors(100, -100)  # 우회전
-    time.sleep(0.8)
+    time.sleep(1)
 
 
 finally:
