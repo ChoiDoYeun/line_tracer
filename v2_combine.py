b44b444ebb55d708370aef23090b5d6451405a47
Choi Do Yeun <87837828+ChoiDoYeun@users.noreply.github.com>
2024-10-06 15:51:32 +0900

    Update v2_combine.py

--- v2_combine.py
+++ v2_combine.py
@@ -242,7 +242,7 @@ def avoid_obstacle_and_return(cap):
     if ld > rd:
         print("좌측 회피")
         control_motors(-50, 50)  # 좌회전
-        time.sleep(0.60)  # 회피 시간 설정
+        time.sleep(0.40)  # 회피 시간 설정
         control_motors(40, 40)  # 직진
         time.sleep(1.25)
         motor1.stop()
@@ -272,7 +272,7 @@ def avoid_obstacle_and_return(cap):
     else:
         print("우측 회피")
         control_motors(50, -50)
-        time.sleep(0.60)  # 회피 시간 설정
+        time.sleep(0.40)  # 회피 시간 설정
         control_motors(40, 40)
         time.sleep(1.25)
         motor1.stop()
