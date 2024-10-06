fd906e167709b417cbebb4acb34b3ddab018a519
Choi Do Yeun <87837828+ChoiDoYeun@users.noreply.github.com>
2024-10-06 14:41:49 +0900

    Update v2_combine.py

--- v2_combine.py
+++ v2_combine.py
@@ -215,7 +215,7 @@ def avoid_obstacle_and_return():
         print("좌측 회피")
         # 좌측으로 회피하며 대각선 이동
         control_motors(-50, 50)
-        time.sleep(0.5)  # 회피 시간 설정
+        time.sleep(1)  # 회피 시간 설정
         control_motors(40, 40)
         time.sleep(1)
         control_motors(50,-50)
@@ -226,7 +226,7 @@ def avoid_obstacle_and_return():
         print("우측 회피")
         # 우측으로 회피하며 대각선 이동
         control_motors(50, -50)
-        time.sleep(0.5)  # 회피 시간 설정
+        time.sleep(1)  # 회피 시간 설정
         control_motors(40, 40)
         time.sleep(1)
         control_motors(-50,50)
