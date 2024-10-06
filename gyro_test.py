0ba0cfecdde112a4e874f3e4758cd8398bc1a700
Choi Do Yeun <87837828+ChoiDoYeun@users.noreply.github.com>
2024-10-06 17:24:29 +0900

    Update gyro_test.py

--- gyro_test.py
+++ gyro_test.py
@@ -1,8 +1,8 @@
 from mpu6050 import mpu6050
 import time
 
-# 센서 초기화
-sensor = mpu6050(0x68)
+# 센서 초기화 (주소 0x1d 사용)
+sensor = mpu6050(0x1d)
 
 while True:
     accel_data = sensor.get_accel_data()
@@ -11,4 +11,4 @@ while True:
     print(f"Accelerometer: {accel_data}")
     print(f"Gyroscope: {gyro_data}")
     
-    time.sleep(1)
+    time.sleep(0.01)
